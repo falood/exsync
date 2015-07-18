d255a1af1f050c5900d137ae109c4338a7d944c9
falood <falood@gmail.com>
2015-07-18 23:52:17 +0800

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExSync.Mixfile do
 
   def project do
     [ app: :exsync,
-      version: "0.0.3",
+      version: "0.1.0",
       elixir: "~> 1.0",
       elixirc_paths: ["lib", "web"],
       deps: deps,
@@ -20,7 +20,7 @@ defmodule ExSync.Mixfile do
   end
 
   defp deps do
-    [ {:exfswatch, "~> 0.0.2"} ]
+    [ {:exfswatch, "~> 0.1"} ]
   end
 
   defp package do
