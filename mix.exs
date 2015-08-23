04b1784a0b86abe6dcb23a7913311f10d28e51c9
falood <falood@gmail.com>
2015-08-23 14:20:38 +0800

    Release v0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExSync.Mixfile do
 
   def project do
     [ app: :exsync,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.0",
       elixirc_paths: ["lib", "web"],
       deps: deps,
