054ee2d69478a2b5fe178373f1552e33156f90e6
Marc A. Paradise <marcparadise@users.noreply.github.com>
2018-09-16 18:27:28 -0400

    Experimental: refine fs events to prevent spurious recompile/loads (#13)
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- lib/exsync/src_monitor.ex
+++ lib/exsync/src_monitor.ex
@@ -16,12 +16,19 @@ defmodule ExSync.SrcMonitor do
     {:ok, %{watcher_pid: watcher_pid}}
   end
 
-  def handle_info(
-        {:file_event, watcher_pid, {path, _events}},
-        %{watcher_pid: watcher_pid} = state
-      ) do
-    if Path.extname(path) in ExSync.Config.src_extensions() do
-      ExSync.Utils.recomplete()
+  def handle_info({:file_event, watcher_pid, {path, events}}, %{watcher_pid: watcher_pid}=state) do
+    if Path.extname(path) in ExSync.Config.src_extensions do
+      # This may also vary based on editor - when saving a file in neovim on linux,
+      # events received ar3e:
+      #   :modified
+      #   :modified, :closed
+      #   :attribute
+      # Rather than coding specific behaviors for each OS, look for the modified event in
+      # isolation to trigger things.
+      # TODO: untested assumption that this behavior is common across Mac/Linux/Win
+      if [:modified] == events do
+        ExSync.Utils.recomplete
+      end
     end
 
     {:noreply, state}
