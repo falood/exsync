368cbcc30a30278e179059ccedb84c8a261bd5c1
Jason Axelson <jason.axelson@gmail.com>
2024-05-05 08:10:19 -1000

    Don't check Mix.env on startup

--- lib/exsync/application.ex
+++ lib/exsync/application.ex
@@ -2,21 +2,6 @@ require Logger
 
 defmodule ExSync.Application do
   def start(_, _) do
-    case Mix.env() do
-      :dev ->
-        start_supervisor()
-
-      _ ->
-        Logger.error("ExSync NOT started. Only `:dev` environment is supported.")
-        {:ok, self()}
-    end
-  end
-
-  def start() do
-    Application.ensure_all_started(:exsync)
-  end
-
-  def start_supervisor do
     children =
       [
         maybe_include_src_monitor(),
@@ -32,6 +17,10 @@ defmodule ExSync.Application do
     ]
 
     Supervisor.start_link(children, opts)
+  end
+
+  def start() do
+    Application.ensure_all_started(:exsync)
   end
 
   def maybe_include_src_monitor do
